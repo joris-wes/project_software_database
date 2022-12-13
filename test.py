6ee19569e88355b398c95a1361cf412ee4ba0880
joris-wes <99329100+joris-wes@users.noreply.github.com>
2022-12-13 11:45:20 +0100

    Changed database address

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import couchdb
 
-couch = couchdb.Server('http://admin:admin@83.128.10.233:5984/')
+couch = couchdb.Server('http://admin:weatherdata@raspberryjoris.tplinkdns.com:5984/')
 
 db = couch['weatherdata']
 
@@ -14,8 +14,8 @@ file7 = {'name' : 'Enschede', 'temp' : '25'}
 file8 = {'name' : 'Gronau', 'temp' : '26'}
 file9 = {'name' : 'Enschede', 'temp' : '27'}
 file10 = {'name' : 'Gronau', 'temp' : '30'}
-db.save(file)
-'''db.save(file2)
+print(db.save(file))
+db.save(file2)
 db.save(file3)
 db.save(file4)
 db.save(file5)
@@ -23,7 +23,7 @@ db.save(file6)
 db.save(file7)
 db.save(file8)
 db.save(file9)
-db.save(file10)'''
+db.save(file10)
 mango = { "selector": {
       "name": "Gronau"
    },
